2fed1a89eaa087baa0668e0cd6e3a8c3a0964206
Yixiao Li <liyixiao7@gmail.com>
2019-03-07 20:02:16 +0900

    add sdk for HRP3

---
+++ common/ev3api/api.cfg.h
@@ -0,0 +1,13 @@
+/**
+ * Header file for 'api.cfg'
+ */
+
+#pragma once
+
+#include "ev3api.h"
+#include "ev3.h"
+
+/**
+ * Application initialize task
+ */
+extern void _app_init_task(intptr_t unused);
